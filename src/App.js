25642dc1bf4cf7f7e796fdddcef0fb873d2d671d
Osiris Román <osirisr1994@gmail.com>
2020-12-17 16:35:48 -0500

    Adding initial components and routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './components/auth/Login/Login';
+import NewAccount from './components/auth/NewAccount/NewAccount';
+import Projects from './components/projects/Projects';
+
 function App() {
-  return <div>MERN Task</div>;
+  return (
+    <Router>
+      {/* Anything outside Switch but inside Router will be shared between all different pages in our web app */}
+      <Switch>
+        {/* Here the diferent pages of our web app are defined */}
+        <Route exact path='/' component={Login} />
+        <Route exact path='/new_account' component={NewAccount} />
+        <Route exact path='/projects' component={Projects} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
