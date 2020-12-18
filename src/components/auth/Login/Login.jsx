2bd04355b6aed959b4ab42de919b463b07e55c3e
Osiris Román <osirisr1994@gmail.com>
2020-12-17 19:06:13 -0500

    Adding the login component.
    The css was improved removing the min heigh attribute. This attribute
    caused the login component look awful in small devices.

--- src/components/auth/Login/Login.jsx
+++ src/components/auth/Login/Login.jsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  //Login state
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onFormChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const login = (e) => {
+    e.preventDefault();
+    //To avoid empty fields
+    if (user.email === '' || user.password === '') return;
+    console.log(
+      `Login with email: '${user.email}' and password: '${user.password}'`
+    );
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='form-usuario'>
+      <div className='contenedor-form sombra-dark'>
+        <h1>Iniciar Sesión</h1>
+
+        <form onSubmit={login}>
+          <div className='campo-form'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Your email here!'
+              value={user.email}
+              onChange={onFormChange}
+            />
+          </div>
+          <div className='campo-form'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Your password here!'
+              value={user.password}
+              onChange={onFormChange}
+            />
+          </div>
+
+          <div className='campo-form'>
+            <input
+              type='submit'
+              className='btn btn-primario btn-block'
+              value='Login'
+            />
+          </div>
+        </form>
+        <Link to={'/new_account'} className='enlace-cuenta'>
+          Create New Account
+        </Link>
+      </div>
     </div>
   );
 };
