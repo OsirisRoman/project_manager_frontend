d2a9f6b5b64b61afbca9a5a03405cf30a1edea5e
Osiris Román <osirisr1994@gmail.com>
2020-12-18 17:24:06 -0500

    Task Form component Added.
    This componente will let the user create a tasks for the different
    projects.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 import Sidebar from '../layout/Sidebar';
 import Navbar from '../layout/Navbar';
+import TaskForm from '../tasks/TaskForm';
 
 const Projects = () => {
   return (
     <div className='contenedor-app'>
       <Sidebar />
+
       <div className='seccion-principal'>
         <Navbar />
+
         <main>
+          <TaskForm />
           <div className='contenedor-tareas'></div>
         </main>
       </div>
